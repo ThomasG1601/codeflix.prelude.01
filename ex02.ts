f30cb97575ac29fac86eed5902c9d874599a383c
ThomasG1601 <thomas.gaudin@efrei.net>
2021-10-04 11:11:51 +0200

    Adding ex02.ts

--- ex02.ts
+++ ex02.ts
@@ -4,8 +4,8 @@ function upperCase(str = '') {
     for (let i=0; i < str.length; i++) {
       const code = str.charCodeAt(i)
       
-      if (code >= 65 && code <= 90) {
-        result += String.fromCharCode(code + 32)
+      if (code >= 97 && code <= ) {
+        result += String.fromCharCode(code - 32)
       } else {
         result += str[i] 
       }
