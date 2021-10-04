51dc53e79c7227162417c31055c24ecba602ad54
ThomasG1601 <thomas.gaudin@efrei.net>
2021-10-04 12:29:35 +0200

    Adding ex03.ts

--- ex03.ts
+++ ex03.ts
@@ -1,3 +1,10 @@
-function length (str ='') : number {
-    return ''
+function lengthfunction (str = '') {
+    let i = 0
+    let result = ''
+
+    while(str[i]!=undefined){
+        i++
+    }
+    return i
 }
+console.log(lengthfunction('ch0pper'))
